c357f6100b893c02380ff9f1b5806c4f20085bfe
MichalGabrick <michalgabrick@utexas.edu>
2023-06-16 00:57:00 -0500

    Changed date col to datetime object & made yearly dataframe

--- portfolio_simulation.py
+++ portfolio_simulation.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import random
-import sklearn
 
 # Constants
-spx_path = 'SPX.csv'
+SPX_PATH = 'SPX.csv'
+START_YEAR = 1928
+END_YEAR = 2020
 
-# Load S&P 500 dataset into dataframe
-spx = pd.read_csv(spx_path)
-spx = spx.drop(['Volume'], axis=1)
-spx['Pct Change'] = (spx['Adj Close'] - spx['Open']) / spx['Open'] * 100
-spx = spx.round(3)
+# Load S&P 500 dataset into dataframe and create returns dataframe
+spx = pd.read_csv(SPX_PATH)
+spx_daily_close = spx[['Date', 'Adj Close']]
+# TODO: spx_yearly_close = 
+
+
+# Create yearly returns dataframe
+#spx_yearly_returns = pd.DataFrame(data, columns=['Year', 'Return']) # TODO: create data source (list of year, return, i.e. 1982, 0.078)
 
 # Create class wrapper to handle specific methods for manipulating dataframe
 class StockTools:
@@ -18,30 +23,48 @@ class StockTools:
     def __init__(self, dataframe):
         self.df = dataframe
         
-    def sample_range(self, years=10, split=None): 
-        '''Sample a time range from a dataframe. Input number of years to sample. \
-            Specify split as a list of integers to sample years in different portions.'''
+    # block bootstrapping to obtain historical data
+    def sample_range(self, years=40, block=None): 
+        '''
+        Sample a time range from a dataframe. \
+        Specify block as a list of integers to sample years in different portions. \
+        Returns a dataframe of daily percent changes of a historical time range.
         
-        # input checking
-        if not isinstance(years, int) or not (split == None or split.isinstance(list)):
+            Parameters:
+                years (int): Number of years to sample.
+                block (list): List of integers as year lengths to sample in blocks.
+        
+            Returns:
+                range (pd.DataFrame): Dataframe of daily percent changes over sampled years.
+        '''
+        
+        # input/type checking
+        if not isinstance(years, int) or not (block == None or block.isinstance(list)):
             print("Incorrect inputs.")
             return
         
-        if split.isinstance(list):
-            if not all(isinstance(x, int) for x in split):
+        if block.isinstance(list):
+            if not all(isinstance(x, int) for x in block):
                 print("Incorrect inputs.")
                 return
             
-        if years > 95:
+        if years > 90:
             print("Year range too long to sample from.")
             return
             
         # main functionality
-        if split:
+        if block:
             pass
             # TODO: implement splitting    
         else:
-            pass 
-            #TODO: implement base sampling
+            start_date = random.randint(START_YEAR, END_YEAR - years)
+            end_date = start_date + years
+            
+            range = spx_yearly_returns.iloc[start_date:end_date+1]
+            
+            return range
+            
 
-print(spx.loc[15000:15005])
+
+### TESTING ###
+print(spx_close.iloc[:35])
