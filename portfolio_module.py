cddb584ced39fac5cc768f816977ff254557aee6
MichalGabrick <michalgabrick@utexas.edu>
2023-07-16 22:02:43 -0500

    Create plots and form function to run sims with meta params

--- portfolio_module.py
+++ portfolio_module.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from random import randint
 
+
 # block bootstrapping to obtain historical data
 def block_bootstrap(
-    sample_data: pd.DataFrame, n: int, block_size: int = 1
+    data: pd.DataFrame, n: int, block_size: int = 1
 ) -> pd.DataFrame:
     """
     Resample data points from a dataframe.
 
         Parameters:
-                sample_data (pd.DataFrame): Dataframe to sample from.
+                data (pd.DataFrame): Dataframe to sample from.
                 n (int): Number of data points to sample.
                 block_size (int): Defines number of data points to sample as a block.
 
@@ -21,26 +22,22 @@ def block_bootstrap(
     if not isinstance(n, int) or not isinstance(block_size, int):
         raise TypeError
 
-    if n > len(sample_data.values):
-        print("Too many datapoints to sample.")
-        raise ValueError
-
     if n % block_size != 0:
         print("Make sure that n is a multiple of block_size.")
         raise ValueError
 
     # create resampled dataframe
-    new_sample = pd.DataFrame(columns=list(sample_data.columns))
+    new_sample = pd.DataFrame(columns=list(data.columns))
 
     # sample (n // block_size) blocks of data points
     for _ in range(n // block_size):
-        # randomly select start and end of a block of data points (used to sample from sample_data)
-        start = randint(0, len(sample_data.values) - block_size - 1)
+        # randomly select start and end of a block of data points (used to sample from data)
+        start = randint(0, len(data.values) - block_size - 1)
         end = start + block_size
 
-        # sample block from sample_data and append to new_sample
+        # sample block from data and append to new_sample
         new_sample = pd.concat(
-            [new_sample, sample_data.iloc[start:end]], ignore_index=True
+            [new_sample, data.iloc[start:end]], ignore_index=True
         )
 
     return new_sample
@@ -48,21 +45,21 @@ def block_bootstrap(
 
 # simulation of an investment over a time range using a dataframe of returns
 def portfolio_sim(
-    stock_historical_returns: pd.DataFrame,
-    bond_historical_returns: pd.DataFrame,
+    stock_returns: pd.DataFrame,
+    bond_returns: pd.DataFrame,
     stock_allocation: int,
     principal: int,
     annual_contribution: int,
     rebalance: bool,
-    rebalancing_band: int,
-):
+    rebalancing_band: int = 0,
+) -> pd.DataFrame:
     """
     Simulate the future values of a portfolio of securities.
 
         Parameters:
-                stock_historical_returns (pd.DataFrame): Dataframe of annual historical returns of stocks.
-                bond_historical_returns (pd.DataFrame): Dataframe of annual historical returns of bonds.
-                stock_allocation (int): Percentage of assets in portfolio allocated to stocks.
+                stock_returns (pd.DataFrame): Dataframe of annual historical returns of stocks.
+                bond_returns (pd.DataFrame): Dataframe of annual historical returns of bonds.
+                stock_allocation (int): Percentage of assets in portfolio allocated to stocks representated as a decimal from 0 to 1.
                 principal (int): Initial value of portfolio.
                 annual_contribution (int): Amount to increase principal by annually.
                 rebalance (bool): Choose whether or not to rebalance portfolio annually.
@@ -76,7 +73,7 @@ def portfolio_sim(
     future_values = pd.DataFrame(
         columns=["Year", "Total Value", "Stock Value", "Bond Value", "Stock Allocation"]
     )
-    future_values.loc[len(future_values)] = {
+    future_values.loc[0] = {
         "Year": 0,
         "Total Value": principal + annual_contribution,
         "Stock Value": (principal + annual_contribution) * stock_allocation,
@@ -85,15 +82,15 @@ def portfolio_sim(
     }
 
     # iterate through historical returns of stocks and bonds, append values for each year in data
-    for x in range(1, len(stock_historical_returns) + 1):
+    for x in range(1, len(stock_returns)): # TODO: MAYBE NEEDS A '+ 1'
         # asset value for 'year n' is (value for 'year n-1' + annual contribution) * asset returns for that year
         stock_value = (
-            future_values.loc(x - 1)["Stock Value"] + annual_contribution
-        ) * stock_historical_returns.loc[x]["Returns"]
+            future_values.iloc[x - 1]["Stock Value"] + (annual_contribution * stock_allocation)
+        ) * (1 + stock_returns.loc[x]["Returns"])
 
         bond_value = (
-            future_values.loc(x - 1)["Bond Value"] + annual_contribution
-        ) * bond_historical_returns.loc[x]["Returns"]
+            future_values.iloc[x - 1]["Bond Value"] + (annual_contribution * (1 - stock_allocation))
+        ) * (1 + bond_returns.loc[x]["Returns"])
 
         current_allocation = stock_value / (stock_value + bond_value)
 
@@ -107,7 +104,7 @@ def portfolio_sim(
                 stock_value = (stock_value + bond_value) * stock_allocation
                 bond_value = (stock_value + bond_value) * (1 - stock_allocation)
 
-        future_values.loc[len(future_values)] = {
+        future_values.loc[x] = {
             "Year": x,
             "Total Value": stock_value + bond_value,
             "Stock Value": stock_value,
--- portfolio_simulation.py
+++ portfolio_simulation.py
@@ -2,11 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
-import time
 import portfolio_module as pm
+import seaborn as sns
 
-
-st = time.time()
 
 # Suppresses warning, fix later
 pd.set_option("mode.chained_assignment", None)
@@ -86,34 +84,159 @@ spx_annual_returns = spx_annual_returns.astype({"Year": "int"})
 uscb_annual_returns = uscb_annual_returns.astype({"Year": "int"})
 
 
-### TESTING/PRINTS ###
+# function to run a set of simulations with meta parameters
+def allocation_sims(config: list, allocations: list, sims: int):
+    sims_by_allocation = pd.DataFrame(dtype="float64")
 
-# print(uscb_annual_returns.to_string())
-# print(pm.block_bootstrap(uscb_annual_returns, 10, 1))
+    for x in allocations:
+        final_values = pd.Series(dtype="float64")
 
-### RUN SIMS OF STOCK
-""" sims = []
+        for _ in range(sims):
+            # sample stock and bond dataframes
+            spx_bootstrap = pm.block_bootstrap(
+                data=spx_annual_returns, n=config[0], block_size=5
+            )
+            uscb_bootstrap = pm.block_bootstrap(
+                data=uscb_annual_returns, n=config[0], block_size=5
+            )
 
-for _ in range(1000):
-    sims.append(pm.block_bootstrap(sample_data=spx_annual_returns, n=40, block_size=5))
+            # simulate a portfolio with x percentage stock allocation
+            sim = pm.portfolio_sim(
+                spx_bootstrap,
+                uscb_bootstrap,
+                x,
+                1000,
+                config[1],
+                rebalance=config[2],
+                rebalancing_band=config[2],
+            )
 
-et = time.time()
+            # save final value of each simulation in final_values series
+            final_values.loc[len(final_values)] = sim.loc[len(sim) - 1]["Total Value"]
 
-elapsed = et - st
-print("Execution time:", elapsed, "seconds")
+        # store each final_value series as a row, differentiated by allocation
+        sims_by_allocation = pd.concat(
+            [sims_by_allocation, final_values.to_frame().T], ignore_index=True
+        )
 
-### TESTING RETURNS
-ret = []
+    # rename indices to represent asset allocations
+    sims_by_allocation = sims_by_allocation.rename(
+        index={
+            a: f"{int(b * 100)}/{int((1-b) * 100)}"
+            for (a, b) in zip(list(range(len(sims_by_allocation))), allocations)
+        }
+    )
 
-for x in sims:
-    total_return = 1
-    returns = x["Returns"].tolist()
+    return sims_by_allocation
 
-    for i in returns:
-        total_return *= i + 1
 
-    ret.append(total_return)
+######################### TESTING/PRINTS ############################################
 
-print(sum(ret) / len(ret))
+""" # 20 year period, 0 annual contribution, rebalancing with 5% band
+config_1 = [20, 0, True, 0.05]  
+allocation_values_1 = [0, 0.3, 0.5, 0.7, 1]
+sims_1 = allocation_sims(config_1, allocation_values_1, sims=10000)
 
- """
+# 40 year period, 0 annual contribution, rebalancing with 5% band
+config_2 = [40, 0, True, 0.05]  
+allocation_values_2 = [0, 0.3, 0.5, 0.7, 1]
+sims_2 = allocation_sims(config_2, allocation_values_2, sims=10000)
+
+# 20 year period, 0 annual contribution, no rebalancing
+config_3 = [20, 0, False, 0.05]  
+allocation_values_3 = [0, 0.3, 0.5, 0.7, 1]
+sims_3 = allocation_sims(config_3, allocation_values_3, sims=10000)
+
+# 20 year period, 0 annual contribution, strict rebalancing
+config_4 = [20, 0, True, 0]  
+allocation_values_4 = [0, 0.3, 0.5, 0.7, 1]
+sims_4 = allocation_sims(config_4, allocation_values_4, sims=10000)
+
+# create figure and kdeplot
+fig, ax = plt.subplots(2, 2, figsize=(16, 8), layout="constrained", sharey='row')
+fig.suptitle("Distribution of Portfolio Values", fontsize=16)
+sns.set_palette("colorblind")
+
+sns.kdeplot(ax=ax[0, 0], data=sims_1.T, log_scale=True, lw=2)
+ax[0, 0].set(
+    xlabel="Total Value [$]", title=f"{config_1[0]} Year Period with 5% Rebalancing Band"
+)
+ax[0, 0].get_legend().set_title("Stocks/Bonds")
+
+sns.kdeplot(ax=ax[0, 1], data=sims_2.T, log_scale=True, lw=2)
+ax[0, 1].set(
+    xlabel="Total Value [$]", title=f"{config_2[0]} Year Period with 5% Rebalancing Band"
+)
+ax[0, 1].get_legend().set_title("Stocks/Bonds")
+
+sns.kdeplot(ax=ax[1, 0], data=sims_3.T, log_scale=True, lw=2)
+ax[1, 0].set(
+    xlabel="Total Value [$]", title=f"{config_3[0]} Year Period without Rebalancing"
+)
+ax[1, 0].get_legend().set_title("Stocks/Bonds")
+
+sns.kdeplot(ax=ax[1, 1], data=sims_4.T, log_scale=True, lw=2)
+ax[1, 1].set(
+    xlabel="Total Value [$]", title=f"{config_4[0]} Year Period with Strict Rebalancing"
+)
+ax[1, 1].get_legend().set_title("Stocks/Bonds")
+
+ax[1, 1].annotate(
+    "10000 portfolios per allocation ratio were simulated with 1000 principal and no annual contribution.",
+    xy=(1.0, -0.2),
+    xycoords="axes fraction",
+    ha="right",
+    va="center",
+    fontsize=10,
+)
+
+plt.savefig(f"kde.png")
+plt.show()
+
+for x in [0, .2, .4, .6, .8, 1]:
+    tvals = pd.Series(dtype='float64')
+    
+    for _ in range(1000):
+        spx_bootstrap = pm.block_bootstrap(sample_data=spx_annual_returns, n=60, block_size=5)
+        uscb_bootstrap = pm.block_bootstrap(sample_data=uscb_annual_returns, n=60, block_size=5)
+
+        sim = pm.portfolio_sim(spx_bootstrap, uscb_bootstrap, x, 1000, 0, True, .05)
+
+        tvals.loc[len(tvals)] = sim.loc[39]["Total Value"]
+    
+    print(f'Stock/Bond Split: {x}:{int((10-(x*10)))/10}')
+    print(f'Min: {tvals.min()}')
+    print(f'Max: {tvals.max()}')
+    print(f'Avg: {tvals.mean()}')
+    print(f'SD: {tvals.std()}')
+    print()
+
+# sns lineplot, mean line w/ 95% confidence bound
+simulations = pd.DataFrame(columns=["Year", "Total Value"])
+for _ in range(500):
+    spx_bootstrap = pm.block_bootstrap(sample_data=spx_annual_returns, n=40, block_size=5)
+    uscb_bootstrap = pm.block_bootstrap(sample_data=uscb_annual_returns, n=40, block_size=5)
+    
+    sim = pm.portfolio_sim(spx_bootstrap, uscb_bootstrap, 1, 1000, 0, True, .05)
+    
+    for x in range(len(sim)):
+        simulations.loc[len(simulations)] = {"Year": sim.loc[x]["Year"], "Total Value": sim.loc[x]["Total Value"]}
+
+sns.lineplot(data=simulations, x="Year", y="Total Value")
+plt.show()
+
+# regular multicolor plot
+fig, ax = plt.subplots()
+fig.suptitle("Monte Carlo Simulation of Portfolio Values", fontsize=14)
+sns.set_palette("colorblind")
+ax.set(xlabel='Time', ylabel="Total Value [$]")
+
+for _ in range(100):
+    spx_bootstrap = pm.block_bootstrap(data=spx_annual_returns, n=40, block_size=5)
+    uscb_bootstrap = pm.block_bootstrap(data=uscb_annual_returns, n=40, block_size=5)
+    
+    sim = pm.portfolio_sim(spx_bootstrap, uscb_bootstrap, .5, 1000, 0, True, .05)
+    ax = plt.plot(sim["Year"], sim["Total Value"])
+
+plt.savefig("montecarlo_sim.png")
+plt.show() """
